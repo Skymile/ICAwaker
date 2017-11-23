45a8e9c761b826906f08aa10e31d4a0eaa90481c
Terror Drone <terrosonda@gmail.com>
2017-11-23 02:27:44 +0100

    Init!

---
+++ ICAwaker/ICAwaker/ICAwaker.cpp
@@ -0,0 +1,13 @@
+// Defines the entry point for the application.
+
+#include "stdafx.h"
+
+int main(int argc, char** argv)
+{
+	//	cv::Mat image;
+	cv::namedWindow("Display window", cv::WINDOW_AUTOSIZE);
+	//	imshow("Display window", image); // Show our image inside it.
+
+	cv::waitKey();
+	return 0;
+}
---
+++ ICAwaker/ICAwaker/stdafx.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "targetver.h"
+
+#include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
